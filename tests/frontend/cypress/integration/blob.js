30fee736e2a2a358a83c2f27d59e1b18bfd22268
dsk <dskwhitehat@gmail.com>
2020-07-08 19:03:23 +0200

    Adding blob and checking if it is on the list

--- tests/frontend/cypress/integration/blob.js
+++ tests/frontend/cypress/integration/blob.js
@@ -1,24 +1,46 @@
-describe('Blob test - Malwarecage', function() {
-   it('Blob test', function () {
-       cy.visit('http://malwarefront:80')
+describe("Blob test - Malwarecage", function () {
+  it("Blob test", function () {
+    cy.request("POST", "http://malwarefront:80/api/auth/login", {
+      login: "admin",
+      password: Cypress.env("admin_password"),
+    }).then((response) => {
+      expect(response.body).to.have.property("token");
+      cy.wrap(response.body.token).as("token");
+    });
 
-       cy.url()
-           .should('include', '/login')
+    cy.get("@token").then((token) => {
+      cy.request({
+        method: "PUT",
+        url: "http://malwarefront:80/api/blob/root",
+        body: {
+          blob_name: "some.blob",
+          blob_type: "inject",
+          content: "TEST",
+        },
+        headers: {
+          Authorization: " Bearer " + token,
+        },
+      });
+    });
 
-       cy.get(':nth-child(1) > .form-control')
-           .type('admin')
-           .should('have.value', 'admin')
+    cy.visit("http://malwarefront:80");
 
-       cy.get(':nth-child(2) > .form-control')
-           .type(Cypress.env('admin_password'))
-           .should('have.value', Cypress.env('admin_password'))
+    cy.url().should("include", "/login");
 
-       cy.contains('Submit').click()
-       cy.contains('Logged as: admin')
+    cy.get(":nth-child(1) > .form-control")
+      .type("admin")
+      .should("have.value", "admin");
 
-       cy.contains('Recent blobs').click()
-       cy.contains('some.blob')
+    cy.get(":nth-child(2) > .form-control")
+      .type(Cypress.env("admin_password"))
+      .should("have.value", Cypress.env("admin_password"));
 
-       cy.contains('Logout').click()
-   })
-})
+    cy.contains("Submit").click();
+    cy.contains("Logged as: admin");
+
+    cy.contains("Recent blobs").click();
+    cy.contains("some.blob");
+
+    cy.contains("Logout").click();
+  });
+});
--- tests/frontend/cypress/integration/login.js
+++ tests/frontend/cypress/integration/login.js
@@ -1,21 +1,20 @@
-describe('Login test - Malwarecage', function() {
-   it('Visit malwarecage', function () {
-       cy.visit('http://malwarefront:80')
+describe("Login test - Malwarecage", function () {
+  it("Visit malwarecage", function () {
+    cy.visit("http://malwarefront:80");
 
-       cy.url()
-           .should('include', '/login')
+    cy.url().should("include", "/login");
 
-       cy.get(':nth-child(1) > .form-control')
-           .type('admin')
-           .should('have.value', 'admin')
+    cy.get(":nth-child(1) > .form-control")
+      .type("admin")
+      .should("have.value", "admin");
 
-       cy.get(':nth-child(2) > .form-control')
-           .type(Cypress.env('admin_password'))
-           .should('have.value', Cypress.env('admin_password'))
+    cy.get(":nth-child(2) > .form-control")
+      .type(Cypress.env("admin_password"))
+      .should("have.value", Cypress.env("admin_password"));
 
-       cy.contains('Submit').click()
-       cy.contains('Logged as: admin')
+    cy.contains("Submit").click();
+    cy.contains("Logged as: admin");
 
-       cy.contains('Logout').click()
-   })
-})
+    cy.contains("Logout").click();
+  });
+});

b039070240208a4a3afdf9f4515abd0f0c6050c2
Pawel Srokosz <pawel.srokosz@cert.pl>
2020-08-04 17:28:04 +0200

    Next steps

--- dist/start.sh
+++ dist/start.sh
@@ -6,15 +6,7 @@ until psql "$MALWARECAGE_POSTGRES_URI" -c "\q" ; do
     sleep 1
 done
 
-echo "Applying DB upgrade..."
-flask db upgrade
-
-if [ "$MALWARECAGE_ADMIN_LOGIN" == "" ] || [ "$MALWARECAGE_ADMIN_EMAIL" == "" ] || [ "$MALWARECAGE_ADMIN_PASSWORD" == "" ]
-then
-    echo "Variable MALWARECAGE_ADMIN_LOGIN, MALWARECAGE_ADMIN_EMAIL or MALWARECAGE_ADMIN_PASSWORD is not set. Not going to create initial admin account..."
-else
-    echo "Creating initial admin account..."
-    flask create_admin --require-empty "$MALWARECAGE_ADMIN_LOGIN" "$MALWARECAGE_ADMIN_EMAIL" "$MALWARECAGE_ADMIN_PASSWORD"
-fi
+echo "Configuring Malwarecage instance"
+malwarecage configure --quiet basic
 
 exec uwsgi --ini /app/uwsgi.ini

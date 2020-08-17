e0a8111154781fc5d5116913f99194f9a01a20e5
Paweł Srokosz <pawel.srokosz@cert.pl>
2020-08-17 12:36:15 +0200

    Malwarecage installable from PyPi (#119)
    * Packaged malwarecage - first steps
    
    * Next steps in configuration scripts
    
    * Next steps
    
    * Improved CLI
    
    * Make rate limit working again
    
    * Improved setup.py
    
    * dist => docker
    
    * Fixed bugs
    
    * Next changes
    
    * Bumped version
    
    * Fix CLI message and plugin discovery
    
    * Override cli list
    
    * Some extra comments
    
    * Next update
    
    * Plugin hot-reload doesn't work in new version, need to be restructured

---
+++ malwarecage/version.py
@@ -0,0 +1,8 @@
+try:
+    with open('.git/refs/heads/master', 'r') as f:
+        git_revision = f.read()[:8]
+except IOError:
+    git_revision = ""
+
+app_version = "2.0.0-alpha2"
+app_build_version = f"{app_version}+{git_revision}" if git_revision else app_version

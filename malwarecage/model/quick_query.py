766f1a6d92ae923c3e4597cbc54ace27ef897f18
Krzysztof Wielocha <63305564+KWMORALE@users.noreply.github.com>
2020-08-18 11:40:21 +0200

    Quick query feature (#115)
    * Beta version of back-end for queries
    
    * Beta front-end & api connection
    
    * Working front-end
    
    * Better front-end and fix back-end
    
    * validation errors
    
    * Separate get queries resource
    
    * Delete query modal
    
    * add resources/query docs
    
    * little fix with code location
    
    * fix typing in messages
    
    * Add update quick query feature
    
    * Sorting quick queries by id
    
    * Little style fix and front-end validation
    
    * Add limit to 15 saveed queries at a time
    
    * add error handling in update object resource
    
    * Fix style
    
    * Feature refactor to only delete and add quick query
    
    * renamed query to quick query in model and everywhere
    
    * Don't allow to save broken query
    
    * Fix tests errors
    
    * Update malwarecage/resources/quick_query.py
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/resources/quick_query.py
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/resources/quick_query.py
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/web/src/components/QuickQueryAddModal.js
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/web/src/components/QuickQueryAddModal.js
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/model/migrations/versions/43b78a070936_add_quick_query_model.py
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/model/quick_query.py
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/resources/quick_query.py
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/resources/quick_query.py
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Update malwarecage/model/quick_query.py
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Apply suggestions from code review
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Small bugfix
    
    * Apply suggestions from code review
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Fix add query condition and minor style issues
    
    * Better condition for add quick query
    
    * Update RecentObject.js
    
    * Apply suggestions from code review
    
    Co-authored-by: Paweł Srokosz <pawel.srokosz@cert.pl>
    
    * Better x icon
    
    Co-authored-by: Pawel Srokosz <pawel.srokosz@cert.pl>

---
+++ malwarecage/model/quick_query.py
@@ -0,0 +1,20 @@
+from . import db
+import datetime
+
+
+class QuickQuery(db.Model):
+    __tablename__ = 'quick_query'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    query = db.Column(db.Text, nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    owner = db.relationship('User', lazy='joined')
+
+    @property
+    def owner_login(self):
+        return self.owner.login

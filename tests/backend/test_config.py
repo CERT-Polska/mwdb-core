28f645e7fcb902214998648cf15020965d328168
dsk <dskwhitehat@gmail.com>
2020-08-21 14:19:34 +0200

    Changes for PR

--- tests/backend/test_config.py
+++ tests/backend/test_config.py
@@ -7,32 +7,30 @@ def test_adding_config_with_inblobs():
     Alice = testCase.new_user("Alice", capabilities=["reading_blobs", "adding_blobs"])
 
     config_json = {
-            "cnc": [1,2,3],
-            "raw_cfg": {
+        "cnc": [1, 2, 3],
+        "raw_cfg": {
             "in-blob": {
-            "blob_name": "In blob name",
-            "blob_type": "Blob type",
-            "content": "Blob content"
+                "blob_name": "In blob name",
+                "blob_type": "Blob type",
+                "content": "Blob content"
             }
-            },
-            "peers": {
+        },
+        "peers": {
             "in-blob": {
-            "description": "Desc",
-            "blob_name": "Peers blob name",
-            "blob_type": "Peers blob type",
-            "content": "Hello"
+                "blob_name": "Peers blob name",
+                "blob_type": "Peers blob type",
+                "content": "Hello"
             }
-            }
+        }
     }
 
     config = Alice.session().add_config(None, "malwarex", config_json)
 
-    assert (isinstance(config["cfg"]["raw_cfg"]["in-blob"], str) \
-           and isinstance(config["cfg"]["peers"]["in-blob"], str) \
-           and (config["cfg"]["raw_cfg"]["in-blob"] in [config["children"][0]["id"], config["children"][1]["id"]]) \
-           and (config["cfg"]["peers"]["in-blob"] in [config["children"][0]["id"], config["children"][1]["id"]]))
+    assert sorted([config["cfg"]["raw_cfg"]["in-blob"], config["cfg"]["peers"]["in-blob"]]) == \
+           sorted([config["children"][0]["id"], config["children"][1]["id"]])
 
-    blobs = Alice.session().recent_blobs(1)
+    blob1 = Alice.session().get_blob(config["children"][0]["id"])
+    blob2 = Alice.session().get_blob(config["children"][1]["id"])
 
-    assert blobs["blobs"][0]["blob_name"] in ["In blob name", "Peers blob name"] and \
-           blobs["blobs"][1]["blob_name"] in ["In blob name", "Peers blob name"]
+    assert blob1["blob_name"] in ["In blob name", "Peers blob name"] and \
+           blob2["blob_name"] in ["In blob name", "Peers blob name"]

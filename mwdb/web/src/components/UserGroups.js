0896a6167df3ff50d8f01d95299d7285542e7935
Krzysztof <krzysztof.wielocha@nask.pl>
2020-09-15 10:36:19 +0200

    Action buttons

--- mwdb/web/src/components/UserGroups.js
+++ mwdb/web/src/components/UserGroups.js
@@ -23,32 +23,32 @@ class UserGroupRow extends Component {
     render(){
         return (
             <React.Fragment>
-                <tr>
-                    <th style={{cursor: 'pointer'}} onClick={this._toggle.bind(this)}>
+                <tr className="d-flex">
+                    <th className="col-8" style={{cursor: 'pointer'}} onClick={this._toggle.bind(this)}>
                         <FontAwesomeIcon icon={this.state.open ? "minus" : "plus"} size="sm"/>
                         {" "}
                         <Link to={`/group/${this.props.group.name}`}>
                             <HighlightText>{this.props.group.name}</HighlightText>
                         </Link>
                     </th>
-
-                            <td>
-                                {this.props.group.users.map((c, idx) => [
-                                    <Link key={idx} to={makeSearchLink("uploader", c, false, '')}>
-                                        {c}
-                                    </Link>,
-                                    idx+1 < this.props.group.users.length ? <span>,</span> : ""])
-                                }
-                            </td>
+                    <td className="col-4">
+                        <button type="button" className="btn btn-sm btn-success">
+                                    Add member
+                        </button>
+                    </td>
                 </tr>
                 {this.state.open && (
                     this.props.group.users.map((c, idx) =>
-                            <tr className="nested">
-                                <td colSpan="2">
+                            <tr className="nested d-flex">
+                                <td className="col-8">
                                     <Link key={idx} to={makeSearchLink("uploader", c, false, '')}>
                                         {c}
                                     </Link>
-                                    {/*<UserLink key={idx} login={c}/>*/}
+                                </td>
+                                <td className="col-4 align-middle">
+                                    <button type="button" className="btn btn-sm btn-danger">
+                                        Remove member
+                                    </button>
                                 </td>
                             </tr>
                     ))
@@ -84,17 +84,17 @@ class UserGroups extends Component {
                 <UserGroupRow group ={v}/>
         )
         return (
-            <div className="container-fluid">
+            <div className="container">
                 <ErrorBoundary error={this.state.error}>
                     {this.state.groups.length ?
-                        <table className="table table-striped table-bordered wrap-table">
+                        <table className="table table-bordered table-striped" style={{"border":"1px"}}>
                             <thead>
-                                <tr>
-                                    <th>
+                                <tr className="d-flex">
+                                    <th className="col-8">
                                         Group name
                                     </th>
-                                    <th>
-                                        Members
+                                    <th className="col-4">
+                                        Actions
                                     </th>
                                 </tr>
                             </thead>

e85dbbf997bb98c96269eed91d8bd0df64f7ca9e
dsk <dskwhitehat@gmail.com>
2020-09-21 11:23:37 +0200

    Added extra field to standard logging

--- mwdb/core/log.py
+++ mwdb/core/log.py
@@ -5,11 +5,26 @@ import logmatic
 from .config import app_config
 
 
-class ContextFilter(logging.Filter):
+class ContextFilterJSON(logging.Filter):
     def filter(self, record):
         record.auth_user = g.auth_user.login if g.get('auth_user') is not None else None
         record.request_id = g.request_id
-        print("RECORD" + str(record.__dict__))
+
+        return True
+
+class ContextFilterStandard(logging.Filter):
+    def filter(self, record):
+        record.auth_user = g.auth_user.login if g.get('auth_user') is not None else None
+        record.request_id = g.request_id
+
+        dummy = logging.LogRecord(None, None, None, None, None, None, None)
+        extra_txt = ''
+        for k, v in record.__dict__.items():
+            if k not in dummy.__dict__:
+                extra_txt += '{}:{} - '.format(k, v)
+
+        record.extra = extra_txt
+
         return True
 
 
@@ -25,18 +40,23 @@ handler = logging.StreamHandler()
 
 if logging_type == "standard":
     formatter = logging.Formatter(fmt="File:%(filename)s - Function:%(funcName)s - Level:%(levelname)s - Line:"
-                                      "%(lineno)s - Module:%(module)s - Thread:%(threadName)s - Path:%(path)s - Arguments:%(args)s"
-                                      " Method:%(method)s - Status:%(status)s - Response_time:%(response_time)s - "
-                                      "Response_size:%(response_size)s - Auth_user:%(auth_user)s - Request_id:%(request_id)s"
-                                      " - Message:%(message)s")
+                                      "%(lineno)s - Module:%(module)s - Thread:%(threadName)s" 
+                                      " - Message:%(message)s - %(extra)s")
+    handler.setFormatter(
+        formatter
+    )
+
+    logger.addFilter(ContextFilterStandard())
+
 elif logging_type == "json":
     formatter = logmatic.JsonFormatter(fmt="%(filename) %(funcName) %(levelname) %(lineno) %(module) %(threadName) %(message)")
 
-handler.setFormatter(
-    formatter
-)
+    handler.setFormatter(
+        formatter
+    )
+    logger.addFilter(ContextFilterJSON())
+
 logger.addHandler(handler)
-logger.addFilter(ContextFilter())
 logger.setLevel(logging.INFO)
 
 

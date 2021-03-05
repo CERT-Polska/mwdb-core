41053ce8193c0ce0cf39adb241aa7479e12388f7
Krzysztof <krzysztof.wielocha@nask.pl>
2021-03-05 11:24:09 +0100

    Improvement: Search links instead of admin routes in SharesBox

--- mwdb/web/src/commons/ui/RefString.js
+++ mwdb/web/src/commons/ui/RefString.js
@@ -1,30 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import ObjectLink from "./ObjectLink";
+import { makeSearchLink } from "../helpers";
 
-class RefString extends Component {
-    render() {
-        const reasonText =
-            this.props.reason_type.charAt(0).toUpperCase() +
-            this.props.reason_type.slice(1).toLowerCase();
-        const objLink = this.props.related_object_dhash ? (
-            <ObjectLink
-                type={this.props.related_object_type}
-                id={this.props.related_object_dhash}
-                inline
-            />
-        ) : (
-            <span className="text-muted">(object deleted)</span>
-        );
-        const userLink = (
-            <ObjectLink type="user" id={this.props.related_user_login} />
-        );
+export default function RefString(props) {
+    const reasonText =
+        props.reason_type.charAt(0).toUpperCase() +
+        props.reason_type.slice(1).toLowerCase();
+    const objLink = props.related_object_dhash ? (
+        <ObjectLink
+            type={props.related_object_type}
+            id={props.related_object_dhash}
+            inline
+        />
+    ) : (
+        <span className="text-muted">(object deleted)</span>
+    );
+    const userLink = (
+        <Link to={makeSearchLink("uploader", props.related_user_login)}>
+            {props.related_user_login}
+        </Link>
+    );
 
-        return (
-            <div>
-                {reasonText} {objLink} by {userLink}
-            </div>
-        );
-    }
+    return (
+        <div>
+            {reasonText} {objLink} by {userLink}
+        </div>
+    );
 }
 
-export default RefString;
+// class RefString extends Component {
+//     render() {
+//         const reasonText =
+//             this.props.reason_type.charAt(0).toUpperCase() +
+//             this.props.reason_type.slice(1).toLowerCase();
+//         const objLink = this.props.related_object_dhash ? (
+//             <ObjectLink
+//                 type={this.props.related_object_type}
+//                 id={this.props.related_object_dhash}
+//                 inline
+//             />
+//         ) : (
+//             <span className="text-muted">(object deleted)</span>
+//         );
+//         const userLink = (
+//             <Link
+//                 to={makeSearchLink("uploader", this.props.related_user_login)}
+//             >
+//                 {this.props.related_user_login}
+//             </Link>
+//         );
+//
+//         return (
+//             <div>
+//                 {reasonText} {objLink} by {userLink}
+//             </div>
+//         );
+//     }
+// }
+//
+// export default RefString;

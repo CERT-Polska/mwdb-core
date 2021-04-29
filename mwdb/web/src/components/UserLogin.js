b30a280e6ef237e31ff54aa837be1b8fce5c0868
psrok1 <pawel.srokosz@cert.pl>
2021-04-30 01:05:23 +0200

    UI: Replaced react-autocomplete with custom based on Bootstrap dropdown

--- mwdb/web/src/components/UserLogin.js
+++ mwdb/web/src/components/UserLogin.js
@@ -7,34 +7,6 @@ import api from "@mwdb-web/commons/api";
 import { Extension } from "@mwdb-web/commons/extensions";
 import { View } from "@mwdb-web/commons/ui";
 
-function Autocomplete({items, itemValue, renderItem, value, onChange, ...inputProps}) {
-    const getItemValue = itemValue || ((item) => item)
-    const ItemComponent = renderItem || (({item}) => getItemValue(item));
-
-    return (
-        <div className="dropdown">
-            <div data-toggle="dropdown">
-                <input value={value} onChange={(ev) => onChange(ev.target.value)}
-                       {...(inputProps || {})}
-                />
-            </div>
-            <div className="dropdown-menu" role="menu">
-                {
-                    items.map((item) => (
-                        <button className="dropdown-item"
-                                onClick={(ev) => {
-                                    ev.preventDefault()
-                                    onChange(getItemValue(item));
-                                }}>
-                            <ItemComponent item={item} />
-                        </button>
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
-
 export default function UserLogin() {
     const auth = useContext(AuthContext);
     const history = useHistory();
@@ -89,7 +61,6 @@ export default function UserLogin() {
                         required
                     />
                 </div>
-                <Autocomplete items={["item"]} value={"item"} onChange={()=>{}} className="form-control"/>
                 <nav className="form-group">
                     <Link to="/recover_password">Forgot password?</Link>
                 </nav>

839d0f0e39758ee8d36cafc21ddc3c528ee250d7
Paweł Srokosz <pawel.srokosz@cert.pl>
2021-05-07 13:30:17 +0200

    UI: Profile view improvements (#363)

--- mwdb/web/src/components/Profile/Views/ProfileCapabilities.js
+++ mwdb/web/src/components/Profile/Views/ProfileCapabilities.js
@@ -18,6 +18,7 @@ function CapabilitiesTable({ profile }) {
                                 {capabilitiesList[cap] || "(no description)"}
                             </div>
                             <div>
+                                <small className="text-muted">Got from:</small>
                                 {profile.groups
                                     .filter((group) =>
                                         group.capabilities.includes(cap)
@@ -37,6 +38,8 @@ function CapabilitiesTable({ profile }) {
 export default function ProfileCapabilities({ profile }) {
     return (
         <div className="container">
+            <h2>Capabilities</h2>
+            <p className="lead">Here is the list of account superpowers:</p>
             <CapabilitiesTable profile={profile} />
         </div>
     );

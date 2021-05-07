198b4321484dabba5ec135af6f02857c5ccb4d88
psrok1 <pawel.srokosz@cert.pl>
2021-05-07 17:54:07 +0200

    Apply some suggestions from review

--- mwdb/core/karton.py
+++ mwdb/core/karton.py
@@ -20,41 +20,48 @@ def get_karton_producer() -> Producer:
     )
 
 
-def send_file_to_karton(file):
+def send_file_to_karton(file) -> str:
     from mwdb.model.file import File
 
+    tmpfile = None
+
     try:
-        path = file.get_path()
-        tmpfile = None
-    except Exception:
-        # If get_path doesn't work: download content to NamedTemporaryFile
-        tmpfile = tempfile.NamedTemporaryFile()
-        file_stream = file.open()
-        shutil.copyfileobj(file_stream, tmpfile)
-        File.close(file_stream)
-        path = tmpfile.name
+        # TODO: Use file.open() directly when Resource(fd=...)
+        # is implemented in Karton
+        try:
+            # If file contents are available via path: just use the path
+            path = file.get_path()
+        except (ValueError, IOError):
+            # If get_path doesn't work: download content to NamedTemporaryFile
+            tmpfile = tempfile.NamedTemporaryFile()
+            file_stream = file.open()
+            shutil.copyfileobj(file_stream, tmpfile)
+            File.close(file_stream)
+            path = tmpfile.name
 
-    producer = get_karton_producer()
-    feed_quality = g.auth_user.feed_quality
-    task_priority = TaskPriority.NORMAL if feed_quality == "high" else TaskPriority.LOW
-    task = Task(
-        headers={"type": "sample", "kind": "raw", "quality": feed_quality},
-        payload={
-            "sample": Resource(file.file_name, path=path, sha256=file.sha256),
-            "attributes": file.get_metakeys(as_dict=True, check_permissions=False),
-        },
-        priority=task_priority,
-    )
-    producer.send_task(task)
-
-    if tmpfile is not None:
-        tmpfile.close()
+        producer = get_karton_producer()
+        feed_quality = g.auth_user.feed_quality
+        task_priority = (
+            TaskPriority.NORMAL if feed_quality == "high" else TaskPriority.LOW
+        )
+        task = Task(
+            headers={"type": "sample", "kind": "raw", "quality": feed_quality},
+            payload={
+                "sample": Resource(file.file_name, path=path, sha256=file.sha256),
+                "attributes": file.get_metakeys(as_dict=True, check_permissions=False),
+            },
+            priority=task_priority,
+        )
+        producer.send_task(task)
+    finally:
+        if tmpfile is not None:
+            tmpfile.close()
 
     logger.info("File sent to Karton with %s", task.root_uid)
     return task.root_uid
 
 
-def send_config_to_karton(config):
+def send_config_to_karton(config) -> str:
     producer = get_karton_producer()
     task = Task(
         headers={"type": "config", "kind": config.config_type, "family": config.family},
@@ -70,12 +77,12 @@ def send_config_to_karton(config):
     return task.root_uid
 
 
-def send_blob_to_karton(blob):
+def send_blob_to_karton(blob) -> str:
     producer = get_karton_producer()
     task = Task(
         headers={"type": "blob", "kind": blob.blob_type},
         payload={
-            "config": blob.content,
+            "content": blob.content,
             "dhash": blob.dhash,
             "attributes": blob.get_metakeys(as_dict=True, check_permissions=False),
         },

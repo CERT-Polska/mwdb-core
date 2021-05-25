06cdfb248499270d1f7c74b0caff66260e5ce474
Paweł Srokosz <pawel.srokosz@cert.pl>
2021-05-25 19:47:09 +0200

    UI: GroupsList and UsersList improvements

--- mwdb/web/src/components/Settings/Views/GroupsList.js
+++ mwdb/web/src/components/Settings/Views/GroupsList.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import api from "@mwdb-web/commons/api";
 import {
+    LimitTo,
     UserBadge,
     PagedList,
     HighlightText,
@@ -20,15 +21,17 @@ function GroupItem(props) {
                 </Link>
             </td>
             <td>
-                {props.name === "public"
-                    ? "(Group is public and contains all members)"
-                    : props.users.map((login) => (
-                          <UserBadge
-                              user={{ login }}
-                              clickable
-                              basePath="/admin"
-                          />
-                      ))}
+                <LimitTo count={5}>
+                    {props.name === "public"
+                        ? "(Group is public and contains all members)"
+                        : props.users.map((login) => (
+                              <UserBadge
+                                  user={{ login }}
+                                  clickable
+                                  basePath="/admin"
+                              />
+                          ))}
+                </LimitTo>
             </td>
         </tr>
     );

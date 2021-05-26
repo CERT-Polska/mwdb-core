46e223854de6d494c7e4cf3cea2e5253df35d325
Krzysztof <krzysztof.wielocha@nask.pl>
2021-05-26 10:51:45 +0200

    Removed unnecessary imports

--- mwdb/web/src/components/Navigation.js
+++ mwdb/web/src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import {
--- mwdb/web/src/components/Settings/SettingsView.js
+++ mwdb/web/src/components/Settings/SettingsView.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { NavLink, Route, Switch } from "react-router-dom";
 
 import { faUsersCog } from "@fortawesome/free-solid-svg-icons";

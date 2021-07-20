c14a8e9a4aded877644895210a2f737cfeff4fe8
Paweł Srokosz <pawel.srokosz@cert.pl>
2021-07-20 18:34:30 +0200

    Convert attributes type to JSONB

--- mwdb/model/metakey.py
+++ mwdb/model/metakey.py
@@ -2,6 +2,7 @@ from string import Template
 
 from flask import g
 from sqlalchemy import UniqueConstraint
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.exc import IntegrityError
 
 from mwdb.core.capabilities import Capabilities
@@ -21,7 +22,7 @@ class Metakey(db.Model):
         nullable=False,
         index=True,
     )
-    value = db.Column(db.Text, nullable=False, index=True)
+    value = db.Column(JSONB, nullable=False, index=True)
     template = db.relationship("MetakeyDefinition", lazy="joined")
 
     @property

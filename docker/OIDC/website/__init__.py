e06d47220e781a1783923bb536489fdac0d88e07
Paweł Srokosz <pawel.srokosz@cert.pl>
2021-09-20 14:02:16 +0200

    Dev: replace toy OIDC server with Keycloak

--- docker/OIDC/website/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-# static files are disabled as this is more of an api
-app = Flask(__name__, static_folder=None)
-
-from website import models, oauth2, settings, views
--- docker/OIDC/website/run.py
+++
@@ -1,4 +0,0 @@
-from website import app as application
-
-if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=5000)

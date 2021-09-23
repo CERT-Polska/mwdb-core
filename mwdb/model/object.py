13004792db90624e1034c804d645588e63a5c185
wklimek <82975932+wklimek@users.noreply.github.com>
2021-09-23 21:58:40 +0200

    PUT /api/{type}/{identifier}/karton/{analysis_id} endpoint parameters and bugfix (#456)

--- mwdb/model/object.py
+++ mwdb/model/object.py
@@ -855,7 +855,7 @@ class Object(db.Model):
             existing.id for existing in self.analyses
         ]:
             self.analyses.append(analysis)
-            return analysis, True
+
         if commit:
             db.session.commit()
         return analysis, is_new

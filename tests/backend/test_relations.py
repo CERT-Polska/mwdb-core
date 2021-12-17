73bd1b4f084d142b30ec0abc52b95635b5ae05f4
Waldemar Klimek <waldemar.klimek@nask.pl>
2021-12-17 14:33:20 +0100

    Tests for conflict situations for ObjectChildResource endpoint

--- tests/backend/test_relations.py
+++ tests/backend/test_relations.py
@@ -1,4 +1,5 @@
 from .relations import *
+from .utils import ShouldRaise, base62uuid
 
 
 def test_inheritance(admin_session):
@@ -465,3 +466,35 @@ def test_removing_relations(admin_session):
         should_access=[Alice],
         should_not_access=[Bob],
     ).test()
+
+
+def test_removing_not_existing_relation(admin_session):
+    test = admin_session
+    
+    filename_1 = base62uuid()
+    file_content_1 = base62uuid()
+    sample_1 = test.add_sample(filename_1, file_content_1)
+    
+    filename_2 = base62uuid()
+    file_content_2 = base62uuid()
+    sample_2 = test.add_sample(filename_2, file_content_2)
+    
+    with ShouldRaise(status_code=409):
+        test.remove_parent(sample_1['sha256'], sample_2['sha256'])
+
+
+def test_add_existing_relation(admin_session):
+    test = admin_session
+    
+    filename_1 = base62uuid()
+    file_content_1 = base62uuid()
+    sample_1 = test.add_sample(filename_1, file_content_1)
+    
+    filename_2 = base62uuid()
+    file_content_2 = base62uuid()
+    sample_2 = test.add_sample(filename_2, file_content_2)
+    
+    test.add_xref(sample_1['sha256'], sample_2['sha256'])
+    
+    with ShouldRaise(status_code=409):
+        test.add_xref(sample_1['sha256'], sample_2['sha256'])

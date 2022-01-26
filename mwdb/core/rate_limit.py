9401be396d0a82663c7742efd8e3f8dd7adaee66
Waldemar Klimek <waldemar.klimek@nask.pl>
2022-01-26 15:15:27 +0100

    Add custom 429 message

--- mwdb/core/rate_limit.py
+++ mwdb/core/rate_limit.py
@@ -1,37 +1,8 @@
-import time
-
-import redis
 from flask import g, request
 from flask_limiter import Limiter
-from werkzeug.exceptions import TooManyRequests
 
 from mwdb.core.capabilities import Capabilities
 from mwdb.core.config import app_config
-
-
-def rate_limit(key, duration, limit):
-    class TooManyRequestsWithRetryAfter(TooManyRequests):
-        def __init__(self, retry_after):
-            self.retry_after = retry_after
-            super().__init__(
-                f"You are too fast. Wait {retry_after} seconds before next request."
-            )
-
-        def get_headers(self, environ=None):
-            return [
-                *super().get_headers(environ=environ),
-                ("Retry-After", self.retry_after),
-            ]
-
-    conn = redis.from_url(app_config.mwdb.redis_uri)
-    current_time = time.time()
-    rate_key = f"{key}-{g.auth_user.login}:{duration}:{current_time // duration}"
-
-    count = conn.incr(rate_key)
-    conn.expire(rate_key, duration)
-
-    if count > limit:
-        raise TooManyRequestsWithRetryAfter(duration - int(current_time % duration))
 
 
 def rate_limit_enabled():
@@ -55,7 +26,7 @@ limiter = Limiter(
 
 def get_limit_decorators(resource):
     # default rate limit values
-    limits_dir = {
+    rate_limits = {
         "get": "1000/10second 2000/minute 6000/5minute 10000/15minute",
         "post": "100/10second 1000/minute 3000/5minute 6000/15minute",
         "put": "100/10second 1000/minute 3000/5minute 6000/15minute",
@@ -67,12 +38,20 @@ def get_limit_decorators(resource):
         if _resource == resource:
             limits = app_config.get_key("mwdb_limiter", field)
             if limits is not None:
-                limits_dir[method] = limits
+                rate_limits[method] = limits
 
     limit_decorators = []
 
-    for key, value in limits_dir.items():
+    for key, value in rate_limits.items():
+        method = key.upper()
         for limit in value.split():
-            limit_decorators.append(limiter.limit(limit, methods=[key]))
+            limit_decorators.append(
+                limiter.limit(
+                    limit,
+                    methods=[method],
+                    error_message=f"Request limit: {limit} "
+                    f"for {method} method was exceeded!",
+                )
+            )
 
     return limit_decorators

bd2ab6e159b665cf473c197d3504927b5328762a
krzysztof.wielocha@nask.pl <krzysztof.wielocha@nask.pl>
2022-06-15 11:29:51 +0200

    UI Refactor: PagedList

--- mwdb/web/src/commons/ui/PagedList.js
+++ mwdb/web/src/commons/ui/PagedList.js
@@ -1,72 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 import Pagination from "react-js-pagination";
 
-class PagedList extends Component {
-    render() {
-        let ListItem = this.props.listItem;
-        let columnNames = this.props.columnNames || ListItem.columnNames;
-        return (
-            <div
-                className="table-responsive"
-                style={{ ...this.props.tableStyle }}
-            >
-                {this.props.onFilterChange ? (
-                    <input
-                        type="text"
-                        name="page-filter"
-                        value={this.props.filterValue}
-                        onChange={this.props.onFilterChange}
-                        className="form-control"
-                        placeholder="Filter by..."
-                    />
-                ) : (
-                    []
-                )}
-                <table className="table table-striped table-bordered wrap-table">
-                    <thead>
+export default function PagedList(props) {
+    const ListItem = props.listItem;
+    const columnNames = props.columnNames || ListItem.columnNames;
+    return (
+        <div className="table-responsive" style={{ ...props.tableStyle }}>
+            {props.onFilterChange ? (
+                <input
+                    type="text"
+                    name="page-filter"
+                    value={props.filterValue}
+                    onChange={props.onFilterChange}
+                    className="form-control"
+                    placeholder="Filter by..."
+                />
+            ) : (
+                []
+            )}
+            <table className="table table-striped table-bordered wrap-table">
+                <thead>
+                    <tr>
+                        {columnNames.map((h, idx) => (
+                            <th key={idx}>{h}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.items.length ? (
+                        props.items.map((item, idx) => (
+                            <ListItem
+                                key={idx}
+                                {...item}
+                                filterValue={props.filterValue}
+                            />
+                        ))
+                    ) : (
                         <tr>
-                            {columnNames.map((h, idx) => (
-                                <th key={idx}>{h}</th>
-                            ))}
+                            <td
+                                colSpan={columnNames.length}
+                                className="text-center"
+                            >
+                                Nothing to display.
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.items.length ? (
-                            this.props.items.map((item, idx) => (
-                                <ListItem
-                                    key={idx}
-                                    {...item}
-                                    filterValue={this.props.filterValue}
-                                />
-                            ))
-                        ) : (
-                            <tr>
-                                <td
-                                    colSpan={columnNames.length}
-                                    className="text-center"
-                                >
-                                    Nothing to display.
-                                </td>
-                            </tr>
-                        )}
-                    </tbody>
-                </table>
-                {this.props.itemCount > 10 ? (
-                    <Pagination
-                        activePage={this.props.activePage}
-                        itemsCountPerPage={10}
-                        totalItemsCount={this.props.itemCount}
-                        pageRangeDisplayed={5}
-                        onChange={this.props.onPageChange}
-                        itemClass="page-item"
-                        linkClass="page-link"
-                    />
-                ) : (
-                    []
-                )}
-            </div>
-        );
-    }
+                    )}
+                </tbody>
+            </table>
+            {props.itemCount > 10 ? (
+                <Pagination
+                    activePage={props.activePage}
+                    itemsCountPerPage={10}
+                    totalItemsCount={props.itemCount}
+                    pageRangeDisplayed={5}
+                    onChange={props.onPageChange}
+                    itemClass="page-item"
+                    linkClass="page-link"
+                />
+            ) : (
+                []
+            )}
+        </div>
+    );
 }
-
-export default PagedList;

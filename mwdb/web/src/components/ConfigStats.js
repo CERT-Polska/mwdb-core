b9a556a2291a6b475724548da494e1f316ca3815
krzysztof.wielocha@nask.pl <krzysztof.wielocha@nask.pl>
2022-06-21 11:20:08 +0200

    UI refactor: ConfigStats

--- mwdb/web/src/components/ConfigStats.js
+++ mwdb/web/src/components/ConfigStats.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 import api from "@mwdb-web/commons/api";
@@ -29,76 +29,77 @@ function ConfigStatsItem(props) {
     );
 }
 
-class ConfigStats extends Component {
-    state = {
-        families: [],
-        sortOrder: [0, 1],
-        filterValue: "*",
-    };
+export default function ConfigStats() {
+    const [families, setFamilies] = useState([]);
+    const [sortOrder, setSortOrder] = useState([0, 1]);
+    const [filterValue, setFilterValue] = useState("*");
+    const [error, setError] = useState("");
+    const didMount = useRef(false);
 
-    async updateStats() {
+    async function updateStats() {
         try {
-            let response = await api.getConfigStats(this.state.filterValue);
-            this.setState({ families: response.data.families });
+            let response = await api.getConfigStats(filterValue);
+            setFamilies(response.data.families);
         } catch (error) {
-            this.setState({ error });
+            setError(error);
         }
     }
 
-    componentDidMount = () => {
-        this.updateStats();
-    };
-
-    get items() {
+    const items = () => {
         let columns = ["family", "last_upload", "count"];
-        let sortCriterion = columns[this.state.sortOrder[0]];
-        let sortOrder = this.state.sortOrder[1];
+        let sortCriterion = columns[sortOrder[0]];
+        let sortOrderVariable = sortOrder[1];
 
-        return this.state.families.sort((a, b) => {
-            if (a[sortCriterion] < b[sortCriterion]) return -sortOrder;
-            if (a[sortCriterion] > b[sortCriterion]) return sortOrder;
+        return families.sort((a, b) => {
+            if (a[sortCriterion] < b[sortCriterion]) return -sortOrderVariable;
+            if (a[sortCriterion] > b[sortCriterion]) return sortOrderVariable;
             return 0;
         });
-    }
-
-    onSort = (sortOrder) => {
-        this.setState({ sortOrder });
     };
 
-    onChangeFilter = (ev) => {
-        let value = ev.target.value;
-        this.setState({ filterValue: value }, () => this.updateStats());
+    const onSort = (sortOrder) => {
+        setSortOrder(sortOrder);
     };
 
-    render() {
-        return (
-            <View ident="configStats" error={this.state.error}>
-                <h2>Global family statistics</h2>
-                <div>
-                    Show stats from:
-                    <select
-                        className="custom-select"
-                        value={this.state.filterValue}
-                        onChange={this.onChangeFilter}
-                    >
-                        <option value="*">all time</option>
-                        <option value="24h">last 24 hours</option>
-                        <option value="72h">last 72 hours</option>
-                        <option value="7d">last 7 days</option>
-                        <option value="30d">last 30 days</option>
-                        <option value="90d">last 90 days</option>
-                    </select>
-                </div>
-                <SortedList
-                    listItem={ConfigStatsItem}
-                    items={this.items}
-                    columnNames={["Family", "Last upload", "Unique configs"]}
-                    sortOrder={this.state.sortOrder}
-                    onSort={this.onSort}
-                />
-            </View>
-        );
-    }
+    const onChangeFilter = (ev) => {
+        setFilterValue(ev.target.value);
+    };
+
+    const getStats = useCallback(updateStats, [filterValue]);
+
+    useEffect(() => {
+        if (!didMount.current) {
+            getStats();
+            return;
+        }
+        getStats();
+    }, [getStats, filterValue]);
+
+    return (
+        <View ident="configStats" error={error}>
+            <h2>Global family statistics</h2>
+            <div>
+                Show stats from:
+                <select
+                    className="custom-select"
+                    value={filterValue}
+                    onChange={onChangeFilter}
+                >
+                    <option value="*">all time</option>
+                    <option value="24h">last 24 hours</option>
+                    <option value="72h">last 72 hours</option>
+                    <option value="7d">last 7 days</option>
+                    <option value="30d">last 30 days</option>
+                    <option value="90d">last 90 days</option>
+                </select>
+            </div>
+            <SortedList
+                listItem={ConfigStatsItem}
+                items={items}
+                columnNames={["Family", "Last upload", "Unique configs"]}
+                sortOrder={sortOrder}
+                onSort={onSort}
+            />
+        </View>
+    );
 }
-
-export default ConfigStats;

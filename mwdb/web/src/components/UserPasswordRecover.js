bffb633e7887950fc3077a8689c5c6cb30e51785
krzysztof.wielocha@nask.pl <krzysztof.wielocha@nask.pl>
2022-07-05 16:36:20 +0200

    UI Refactor: UserPasswordRecover.js

--- mwdb/web/src/components/UserPasswordRecover.js
+++ mwdb/web/src/components/UserPasswordRecover.js
@@ -1,124 +1,111 @@
-import React, { Component } from "react";
+import React, { useState, useContext } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
 import api from "@mwdb-web/commons/api";
 import { ConfigContext } from "@mwdb-web/commons/config";
 import { View } from "@mwdb-web/commons/ui";
 
-export default class UserPasswordRecover extends Component {
-    constructor(props) {
-        super(props);
-        this.state = this.initialState;
-    }
+export default function UserPasswordRecover() {
+    const initialState = {
+        login: "",
+        email: "",
+        success: false,
+        error: null,
+        recaptcha: null,
+    };
 
-    static contextType = ConfigContext;
+    const config = useContext(ConfigContext);
+    const [state, setState] = useState(initialState);
 
-    get initialState() {
-        return {
-            login: "",
-            email: "",
-            success: false,
-            dirty: false,
-            error: null,
-            recaptcha: null,
-        };
-    }
-
-    componentDidMount() {
-        if (this.dirty) this.setState(this.initialState);
-    }
-
-    handleInputChange = (event) => {
+    const handleInputChange = (event) => {
         const target = event.target;
         const value =
             target.type === "checkbox" ? target.checked : target.value;
         const name = target.name;
 
-        this.setState({
+        setState({
+            ...state,
             [name]: value,
-            dirty: true,
         });
     };
 
-    async recoverPassword() {
+    async function recoverPassword() {
         try {
             await api.authRecoverPassword(
-                this.state.login,
-                this.state.email,
-                this.state.recaptcha
+                state.login,
+                state.email,
+                state.recaptcha
             );
-            this.setState({
+            setState({
                 success: true,
             });
         } catch (error) {
-            this.setState({ ...this.initialState, error });
+            setState({ ...initialState, error });
         }
     }
 
-    onCaptchaChange = (value) => {
-        this.setState({ recaptcha: value });
+    const onCaptchaChange = (value) => {
+        setState({ ...state, recaptcha: value });
     };
 
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        this.recoverPassword();
+        recoverPassword();
     };
 
-    render() {
-        let success = this.state.success && (
-            <div>Password reset link has been sent to the e-mail address</div>
-        );
+    let success = state.success && (
+        <div>Password reset link has been sent to the e-mail address</div>
+    );
 
-        return (
-            <View success={success} error={success ? null : this.state.error}>
-                <h2>Recover password</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label>Login</label>
-                        <input
-                            type="text"
-                            name="login"
-                            value={this.state.login}
-                            onChange={this.handleInputChange}
-                            className="form-control"
-                            disabled={this.state.success}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleInputChange}
-                            className="form-control"
-                            disabled={this.state.success}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label>
-                            Please enter the information above to recover your
-                            password.
-                        </label>
-                    </div>
-                    {this.context.config["recaptcha_site_key"] ? (
-                        <ReCAPTCHA
-                            sitekey={this.context.config["recaptcha_site_key"]}
-                            onChange={this.onCaptchaChange}
-                        />
-                    ) : (
-                        []
-                    )}
+    return (
+        <View success={success} error={success ? null : state.error}>
+            <h2>Recover password</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Login</label>
                     <input
-                        type="submit"
-                        value="Submit"
-                        className="btn btn-primary"
-                        disabled={this.state.success}
+                        type="text"
+                        name="login"
+                        value={state.login}
+                        onChange={handleInputChange}
+                        className="form-control"
+                        disabled={state.success}
+                        required
                     />
-                </form>
-            </View>
-        );
-    }
+                </div>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={state.email}
+                        onChange={handleInputChange}
+                        className="form-control"
+                        disabled={state.success}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>
+                        Please enter the information above to recover your
+                        password.
+                    </label>
+                </div>
+                {config["recaptcha_site_key"] ? (
+                    <ReCAPTCHA
+                        sitekey={config["recaptcha_site_key"]}
+                        onChange={onCaptchaChange}
+                    />
+                ) : (
+                    []
+                )}
+                <input
+                    type="submit"
+                    value="Submit"
+                    className="btn btn-primary"
+                    disabled={state.success}
+                />
+            </form>
+        </View>
+    );
 }

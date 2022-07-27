b83713cfaa45285ca670f3b998942f53882abece
krzysztof.wielocha@nask.pl <krzysztof.wielocha@nask.pl>
2022-07-27 14:02:48 +0200

    Update Identicon.js

--- mwdb/web/src/commons/ui/Identicon.js
+++ mwdb/web/src/commons/ui/Identicon.js
@@ -1,36 +1,20 @@
 import identicon from "identicon.js";
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 
 import sha1 from "sha1";
 
 export default function Identicon(props) {
-    const [identiconState, setIdenticonState] = useState("");
-    const mounted = useRef();
-
-    const updateState = () => {
-        if (
-            typeof props.data === "undefined" &&
-            typeof props.hash === "undefined"
-        )
-            return;
-        let data = props.hash || sha1(props.data);
-        let options = {
-            margin: parseInt(props.margin, 10) || 0.08,
-            size: parseInt(props.size, 10),
-            format: "svg",
-        };
-        let ident = new identicon(data, options).toString();
-
-        setIdenticonState("data:image/svg+xml;base64," + ident);
+    if (typeof props.data === "undefined" && typeof props.hash === "undefined")
+        return;
+    let data = props.hash || sha1(props.data);
+    let options = {
+        margin: parseInt(props.margin, 10) || 0.08,
+        size: parseInt(props.size, 10),
+        format: "svg",
     };
+    let ident = new identicon(data, options).toString();
 
-    useEffect(() => {
-        if (!mounted.current) {
-            updateState();
-        } else {
-            updateState();
-        }
-    });
+    const identiconState = "data:image/svg+xml;base64," + ident;
 
     return (
         <img

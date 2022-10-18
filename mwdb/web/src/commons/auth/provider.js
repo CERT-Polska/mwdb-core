d37ba01049fed5bdabe22f7c9a7eb0dca4600464
Paweł Srokosz <pawel.srokosz@cert.pl>
2022-10-18 17:37:46 +0200

    Refactor of AuthProvider to minimize re-renders caused by context providers

--- mwdb/web/src/commons/auth/provider.js
+++ mwdb/web/src/commons/auth/provider.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useCallback, useEffect, useMemo, useState} from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import api from "../api";
@@ -6,37 +6,58 @@ import api from "../api";
 import { Capability } from "./capabilities";
 import { AuthContext } from "./context";
 
-const localStorageAuthKey = "user";
+const LOCAL_STORAGE_KEY = "user";
 
 function isSessionValid(authSession) {
-    if (!authSession || !authSession.token)
+    if (!authSession || !authSession.token) {
         // Token is missing
+        console.warn("Session invalid: 'token' has not been found in session data")
         return false;
+    }
     if (
         JSON.parse(atob(authSession.token.split(".")[0])).exp <=
         new Date() / 1000
-    )
+    ) {
         // Token expired
+        console.warn("Session invalid: 'token' expired")
         return false;
-    if (!authSession.login || !authSession.groups || !authSession.capabilities)
-        // Missing other required auth informations
+    }
+    if (!authSession.login || !authSession.groups || !authSession.capabilities) {
+        // Missing other required auth information
+        console.warn("Session invalid: 'token' is valid but other information are missing")
         return false;
+    }
     return true;
 }
 
-function getStoredAuthSession() {
+function getSessionFromStorage() {
+    const storedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if(!storedData)
+        return null;
     try {
-        const sessionData = localStorage.getItem(localStorageAuthKey);
-        if (!sessionData) return null;
-        const storedAuthSession = JSON.parse(sessionData);
-        if (!isSessionValid(storedAuthSession))
-            throw new Error("Invalid session data");
-        return storedAuthSession;
-    } catch (e) {
-        console.error(e);
-        localStorage.removeItem(localStorageAuthKey);
+        const sessionData = JSON.parse(storedData);
+        if(!isSessionValid(sessionData))
+            return null;
+        return sessionData;
+    } catch(e) {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
         return null;
     }
+}
+
+function putSessionToStorage(sessionData) {
+    if (!sessionData) {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+        return false;
+    }
+    if(!isSessionValid(sessionData)) {
+        return false;
+    }
+    localStorage.setItem(
+        LOCAL_STORAGE_KEY,
+        JSON.stringify(sessionData)
+    );
+    return true;
 }
 
 function setTokenForAPI(token) {
@@ -45,78 +66,78 @@ function setTokenForAPI(token) {
         : null;
 }
 
-function useAxiosEffect(func) {
-    /***
-     * This is special kind of effect that runs *before* render (like componentWillMount).
-     * In the same time, it keeps the original useEffect's clean-up behavior.
-     *
-     * It's required to do proper Axios setup (side-effect) before children are rendered
-     * to prevent premature API requests before the auth token is recovered from localStorage.
-     */
-    const cleanup = useRef(null);
-
-    if (cleanup.current === null) {
-        // Run on first render and set the cleanup callback
-        cleanup.current = func() || (() => {});
-    }
-
-    // Setup useEffect to cleanup the effect on unmount
-    useEffect(() => {
-        return () => {
-            if (cleanup.current) cleanup.current();
-        };
-    }, []);
-}
-
-export function AuthProvider(props) {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const [session, _setSession] = useState(getStoredAuthSession());
-    const refreshTimer = useRef(null);
-    const isAuthenticated = !!session;
-
-    function setSession(newSession) {
-        // Internal session setter which updates token used by Axios
-        // before populating new state to the components
-        _setSession(() => {
+function useSession() {
+    /**
+     * Hook for session data
+     * - gets initial state from local storage
+     * - every state change is synchronized with Axios Authorization header
+     **/
+    const [sessionState, setSessionState] = useState(
+        () => {
+            const session = getSessionFromStorage();
+            if(session)
+                setTokenForAPI(session.token);
+            return session;
+        }
+    )
+    const setSession = useCallback((newSession) => {
+        setSessionState(() => {
             setTokenForAPI(newSession && newSession.token);
             return newSession;
-        });
-    }
+        })
+    }, []);
+    // Synchronize session in another tab with changed local session state
+    useEffect(() => {
+        function synchronizeSessionState() {
+            const storedSession = getSessionFromStorage();
+            setSession(storedSession);
+        }
+        window.addEventListener("storage", synchronizeSessionState);
+        return () => {
+            window.removeEventListener("storage", synchronizeSessionState);
+        };
+    }, [setSession]);
+    return [sessionState, setSession]
+}
 
-    function updateSession(newSession) {
+export function AuthProvider({children}) {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [session, setSession] = useSession();
+    const isAuthenticated = !!session;
+    const { login, groups, capabilities } = session || {};
+
+    const updateSession = useCallback((newSession) => {
         // Updates local session state
         // To be used by UserLogin and other authentication frontends
         if (newSession && !isSessionValid(newSession))
             throw new Error("Provided session state is not valid");
-        // Store new session data and notify other windows about change
-        if (newSession)
-            localStorage.setItem(
-                localStorageAuthKey,
-                JSON.stringify(newSession)
-            );
-        else localStorage.removeItem(localStorageAuthKey);
         // Update session state
         setSession(newSession);
-    }
+        // Store new session data in local storage and notify other windows about change
+        putSessionToStorage(newSession);
+    }, [setSession]);
 
-    async function refreshSession() {
-        try {
-            // If not authenticated: just ignore that call
-            // refreshSession is called by 403 Forbidden interceptor
-            if (!isAuthenticated) return;
-            const response = await api.authRefresh();
-            updateSession(response.data);
-        } catch (e) {
-            // It may fail due to short-lasting network problems.
-            // Just log it and try again later.
-            console.error(e);
+    const refreshSession = useCallback(() => {
+        async function doRefreshSession() {
+            try {
+                // If not authenticated: just ignore that call
+                // refreshSession is called by 403 Forbidden interceptor
+                if (!isAuthenticated) return;
+                const response = await api.authRefresh();
+                updateSession(response.data);
+            } catch (e) {
+                // It may fail due to short-lasting network problems.
+                // Just log it and try again later.
+                console.error(e);
+            }
         }
-    }
+        doRefreshSession();
+    }, [isAuthenticated, updateSession]);
 
-    function logout(error) {
+    const logout = useCallback((error) => {
         // Clears session state and redirects user to the UserLogin page
-        let logoutReason = error
+        const logoutReason = error
             ? { error }
             : { success: "User logged out successfully." };
         updateSession(null);
@@ -126,16 +147,14 @@ export function AuthProvider(props) {
                 ...logoutReason,
             },
         });
-    }
+    }, [updateSession, navigate, location]);
 
-    function hasCapability(capability) {
-        return isAuthenticated && session.capabilities.indexOf(capability) >= 0;
-    }
+    const hasCapability = useCallback((capability) => {
+        return capabilities && capabilities.indexOf(capability) >= 0;
+    }, [capabilities]);
 
     // Effect for 401 Not authenticated to handle unexpected session expiration
-    useAxiosEffect(() => {
-        // Initialize Authorization header on mount
-        setTokenForAPI(isAuthenticated && session.token);
+    useEffect(() => {
         // Set 401 Not Authenticated interceptor when AuthProvider is mounted
         const interceptor = api.axios.interceptors.response.use(
             (_) => _,
@@ -153,11 +172,10 @@ export function AuthProvider(props) {
         return () => {
             api.axios.interceptors.response.eject(interceptor);
         };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    });
+    }, [logout]);
 
     // Effect for 403 Forbidden to handle unexpected loss of permissions
-    useAxiosEffect(() => {
+    useEffect(() => {
         // Set 403 Forbidden interceptor when AuthProvider is mounted
         const interceptor = api.axios.interceptors.response.use(
             (_) => _,
@@ -173,54 +191,38 @@ export function AuthProvider(props) {
         return () => {
             api.axios.interceptors.response.eject(interceptor);
         };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    });
+    }, [refreshSession]);
 
     // Effect for periodic session refresh
     useEffect(() => {
-        function setRefreshTimer() {
-            if (refreshTimer.current) return;
-            refreshTimer.current = setInterval(refreshSession, 60000);
-            refreshSession();
-        }
-
-        function clearRefreshTimer() {
-            if (!refreshTimer.current) return;
-            clearInterval(refreshTimer.current);
-            refreshTimer.current = null;
-        }
-
-        // Set timer if user is logged in, clear otherwise
-        (isAuthenticated ? setRefreshTimer : clearRefreshTimer)();
-        return clearRefreshTimer;
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isAuthenticated]);
-
-    // Synchronize session in another window with local session state
-    useEffect(() => {
-        function synchronizeSessionState() {
-            const storedSession = getStoredAuthSession();
-            setSession(storedSession);
-        }
-        window.addEventListener("storage", synchronizeSessionState);
+        if(!isAuthenticated)
+            return;
+        const refreshTimer = setInterval(refreshSession, 1000);
+        // Perform initial session refresh immediately
+        refreshSession();
         return () => {
-            window.removeEventListener("storage", synchronizeSessionState);
-        };
-    }, []);
+            clearInterval(refreshTimer);
+        }
+    }, [isAuthenticated, refreshSession]);
+
+    const providerValue = useMemo(() => {
+        console.log("Changed value")
+        return {
+            user: {login, groups, capabilities},
+            isAuthenticated,
+            isAdmin: hasCapability(Capability.manageUsers),
+            hasCapability,
+            refreshSession,
+            updateSession,
+            logout,
+        }
+    }, [login, groups, capabilities, isAuthenticated, hasCapability, refreshSession, updateSession, logout])
 
     return (
         <AuthContext.Provider
-            value={{
-                user: session,
-                isAuthenticated: !!session,
-                isAdmin: hasCapability(Capability.manageUsers),
-                hasCapability,
-                refreshSession,
-                updateSession,
-                logout,
-            }}
+            value={providerValue}
         >
-            {props.children}
+            {children}
         </AuthContext.Provider>
     );
 }

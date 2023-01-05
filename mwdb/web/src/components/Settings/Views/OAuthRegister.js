6744765845d27f0df610e0b0119232a02f3e5ed1
Repumba <tomasz.trzeciak@nask.pl>
2023-01-05 15:21:38 +0100

    Implement using discovery/configuration endpoint

--- mwdb/web/src/components/Settings/Views/OAuthRegister.js
+++ mwdb/web/src/components/Settings/Views/OAuthRegister.js
@@ -1,12 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useCallback, useEffect } from "react";
 
 import { APIContext } from "@mwdb-web/commons/api/context";
 
-import { useViewAlert } from "@mwdb-web/commons/ui";
+import { ShowIf, useViewAlert } from "@mwdb-web/commons/ui";
 
 export default function OAuthRegister() {
     const api = useContext(APIContext);
     const viewAlert = useViewAlert();
+    const [discoverData, setDiscoverData] = useState();
+    const [discoverURL, setDiscoverURL] = useState();
     const [values, setValues] = useState({
         userinfo_endpoint: "",
         jwks_endpoint: "",
@@ -15,6 +17,7 @@ export default function OAuthRegister() {
         authorization_endpoint: "",
         client_id: "",
         client_secret: "",
+        logout_endpoint: "",
     });
 
     function handleInputChange(event) {
@@ -25,6 +28,24 @@ export default function OAuthRegister() {
             ...prevState,
             [name]: value,
         }));
+    }
+
+    function autoFill() {
+        if (!discoverData) return;
+        console.log(values);
+        const keys = Object.keys(values);
+        for (const i in keys) {
+            values[keys[i]] = discoverData[keys[i]];
+        }
+        if (typeof discoverData["jwks_uri"] === "undefined")
+            discoverData["jwks_uri"] = "";
+
+        if (typeof discoverData["end_session_endpoint"] === "undefined")
+            discoverData["end_session_endpoint"] = "";
+
+        values["jwks_endpoint"] = discoverData["jwks_uri"];
+        values["logout_endpoint"] = discoverData["end_session_endpoint"];
+        setDiscoverURL("");
     }
 
     async function registerProvider() {
@@ -47,9 +68,84 @@ export default function OAuthRegister() {
         }
     }
 
+    async function updateDiscoverData() {
+        try {
+            var req = new Request(discoverURL);
+            fetch(req)
+                .then(async (response) => {
+                    return await response.json();
+                })
+                .then((x) => {
+                    setDiscoverData(x);
+                });
+        } catch (e) {
+            viewAlert.setAlert({ e });
+        }
+    }
+
+    const getDiscover = useCallback(updateDiscoverData, [
+        discoverURL,
+        viewAlert,
+    ]);
+    const autoFillCallback = useCallback(autoFill, [
+        discoverData,
+        values,
+        viewAlert,
+    ]);
+
+    useEffect(() => {
+        getDiscover();
+    }, [getDiscover]);
+    useEffect(() => {
+        autoFillCallback();
+    }, [autoFillCallback]);
+
     return (
         <div className="container">
             <h2>Register new identity provider</h2>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    setDiscoverURL(e.target.discover_endpoint.value);
+                }}
+            >
+                <div className="form-group">
+                    <label>Discover endpoint</label>
+                    <input
+                        type="text"
+                        name="discover_endpoint"
+                        className="form-control"
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="submit"
+                        value="Get data"
+                        className="btn btn-secondary"
+                    />
+                </div>
+                <ShowIf condition={discoverData}>
+                    <textarea
+                        type="text"
+                        rows="10"
+                        width="100%"
+                        className="form-control"
+                        value={JSON.stringify(discoverData, null, 4)}
+                        disabled
+                    />
+                    <button
+                        className="btn btn-secondary"
+                        onClick={() => {
+                            setDiscoverURL("");
+                            setDiscoverData(null);
+                        }}
+                    >
+                        Hide output
+                    </button>
+                </ShowIf>
+            </form>
+
             <form
                 onSubmit={(ev) => {
                     ev.preventDefault();

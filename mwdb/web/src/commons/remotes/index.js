44c5789a20c3a545c1feec7bbcc38cb9c17ab6c5
Krzysztof Wielocha <63305564+KWMORALE@users.noreply.github.com>
2023-01-23 14:53:49 +0100

    Fix: remote instance path (#739)

--- mwdb/web/src/commons/remotes/index.js
+++ mwdb/web/src/commons/remotes/index.js
@@ -1,9 +1,13 @@
-import { useMatch } from "react-router-dom";
+import { useLocation, matchPath } from "react-router-dom";
 
 export function useRemote() {
     // Returns current remote name or undefined if current view is local
-    const match = useMatch("/remote/:remote");
-    return match && match.params.remote;
+    const location = useLocation();
+    const remotePath = matchPath(
+        { path: "/remote/:remote", end: false },
+        location.pathname
+    );
+    return remotePath && remotePath.params.remote;
 }
 
 export function useRemotePath() {

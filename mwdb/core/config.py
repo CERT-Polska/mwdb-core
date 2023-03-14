4a4abba81b93605b29cf6a452886a49f8f6f9d50
Tomek Chytry-Trzeciak <75318192+Repumba@users.noreply.github.com>
2023-03-14 17:10:49 +0100

    Configurable upload size (#756)

--- mwdb/core/config.py
+++ mwdb/core/config.py
@@ -70,6 +70,10 @@ class MWDBConfig(Config):
     )
     # File upload timeout
     file_upload_timeout = key(cast=int, required=False, default=60000)
+    # Maximum upload size, default is None (no limit)
+    # This value refers to whole upload request ('Content-Length' from request header)
+    # Maximum file size is smaller than that by +/- 500 B
+    max_upload_size = key(cast=int, required=False, default=None)
     # Folder for uploads
     uploads_folder = key(cast=path, required=False)
     # Should we break up the uploads into different folders for example:

6c9bbce26f5e4b5d2169af0b40af34e64996f015
Repumba <t.a.trzeciak@gmail.com>
2023-03-15 14:09:28 +0100

    fix migrations

--- mwdb/model/migrations/versions/3c610b0ddebc_add_related_files_table.py
+++ mwdb/model/migrations/versions/3c610b0ddebc_add_related_files_table.py
@@ -1,7 +1,7 @@
 """add related_files table
 
 Revision ID: 3c610b0ddebc
-Revises: c7c72fd7fac5
+Revises: 717b5da712b8
 Create Date: 2022-12-12 09:02:40.406370
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "3c610b0ddebc"
-down_revision = "c7c72fd7fac5"
+down_revision = "717b5da712b8"
 branch_labels = None
 depends_on = None
 

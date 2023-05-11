3e8da740bc2095975e7268d814bb20d0ff85b705
postrowinski <p.ostrowinski@gmail.com>
2023-05-11 10:11:35 +0200

    init commit

--- mwdb/web/src/components/Profile/Views/ProfileAPIKeys.jsx
+++ mwdb/web/src/components/Profile/Views/ProfileAPIKeys.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useLocation, useOutletContext } from "react-router-dom";
 

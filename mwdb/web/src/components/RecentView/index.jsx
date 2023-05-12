31c33e787e19336cc4c7d104722ea347ac6cc4ef
postrowinski <p.ostrowinski@gmail.com>
2023-05-12 09:41:32 +0200

    init commit to rewrite recent view

--- mwdb/web/src/components/RecentView/index.jsx
+++ mwdb/web/src/components/RecentView/index.jsx
@@ -1,3 +1,4 @@
+//TODO: rewrite folder to tsx
 export { default as RecentView } from "./RecentView";
 export { default as RecentRow } from "./RecentRow";
 export { default as RecentInnerRow } from "./RecentInnerRow";

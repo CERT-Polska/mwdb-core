cd9dbcbe517cd15234096d60d4919077da33e52b
Paweł Srokosz <pawel.srokosz@cert.pl>
2023-07-05 12:44:46 +0200

    Support for PRELOAD_APP environment variable

--- docker/gunicorn.conf.py
+++ docker/gunicorn.conf.py
@@ -3,5 +3,6 @@ import os
 wsgi_app = "mwdb.app:app"
 bind = "0.0.0.0:8080"
 user = "nobody"
+preload_app = bool(int(os.getenv("PRELOAD_APP", "0")))
 reload = bool(int(os.getenv("HOT_RELOAD", "0")))
 workers = int(os.getenv("GUNICORN_WORKERS", "4"))

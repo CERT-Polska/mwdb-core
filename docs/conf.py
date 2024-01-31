606f7c7e0b93cf85c3df00f2337513adc49a8698
Paweł Srokosz <pawel.srokosz@cert.pl>
2024-01-31 16:49:16 +0100

    Bump version: v2.11.0 (#903)

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, CERT Polska'
 author = 'CERT Polska'
 
 # The full version, including alpha/beta/rc tags
-release = '2.10.3'
+release = '2.11.0'
 
 
 # -- General configuration ---------------------------------------------------

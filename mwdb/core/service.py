451ea15dea83ac4a80a08c0f9ee40d6c7faa797e
Paweł Srokosz <pawel.srokosz@cert.pl>
2024-05-16 17:17:48 +0200

    Fix: don't jsonify view method result in Resource (#937)

--- mwdb/core/service.py
+++ mwdb/core/service.py
@@ -14,7 +14,6 @@ from werkzeug.exceptions import (
     MethodNotAllowed,
     ServiceUnavailable,
 )
-from werkzeug.wrappers import Response
 
 from mwdb.version import app_version
 
@@ -46,9 +45,9 @@ class Resource(MethodView):
                 description="Method is not allowed for this endpoint",
             )
         response = getattr(self, method)(*args, **kwargs)
-        if isinstance(response, Response):
-            return response
-        return jsonify(response)
+        if response is None:
+            return jsonify(None), 200
+        return response
 
 
 class Service:

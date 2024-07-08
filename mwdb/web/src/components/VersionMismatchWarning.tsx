4e10f1fa85b6fb7d3fa8c0b5e5e6ba30924ede0b
psrok1 <pawel.srokosz@cert.pl>
2024-07-08 16:14:45 +0200

    Strip possible version tag

--- mwdb/web/src/components/VersionMismatchWarning.tsx
+++ mwdb/web/src/components/VersionMismatchWarning.tsx
@@ -3,11 +3,21 @@ type VersionMismatchWarningProps = {
     clientVersion: string;
 };
 
+function stripVersionTag(version: string): string {
+    // Strips possible version tag with Git revision
+    // e.g. 2.0.0-rc1+6b2f1c1a is converted to 2.0.0-rc1
+    return version.split("+")[0];
+}
+
 export function VersionMismatchWarning({
     serverVersion,
     clientVersion,
 }: VersionMismatchWarningProps) {
-    if (!serverVersion || clientVersion == serverVersion) return <></>;
+    if (
+        !serverVersion ||
+        stripVersionTag(clientVersion) === stripVersionTag(serverVersion)
+    )
+        return <></>;
     return (
         <div className="alert alert-warning" role="alert">
             Server version was recently updated to {serverVersion} while

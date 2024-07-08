9553e8016bbf8f595dc220e39bc598e34b75b80d
psrok1 <pawel.srokosz@cert.pl>
2024-07-08 15:57:03 +0200

    Apply linter

--- mwdb/web/src/components/VersionMismatchWarning.tsx
+++ mwdb/web/src/components/VersionMismatchWarning.tsx
@@ -11,8 +11,8 @@ export function VersionMismatchWarning({
     return (
         <div className="alert alert-warning" role="alert">
             Server version was recently updated to {serverVersion} while
-            currently loaded web application is {clientVersion}. Press
-            CTRL+F5 to clear cache and reload the webpage.
+            currently loaded web application is {clientVersion}. Press CTRL+F5
+            to clear cache and reload the webpage.
         </div>
     );
 }

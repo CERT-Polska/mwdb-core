90ee2815cc0f814b21f4fdbbed09626a0022ac4b
psrok1 <pawel.srokosz@cert.pl>
2025-07-29 17:46:31 +0200

    Bump version: v2.15.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Under the hood of mwdb.cert.pl service hosted by CERT.pl.
 """
 
 setup(name="mwdb-core",
-      version="2.15.0",
+      version="2.15.1",
       description="MWDB Core malware database",
       long_description=LONG_DESCRIPTION,
       author="CERT Polska",

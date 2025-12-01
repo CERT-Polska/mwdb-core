16a768ea6beda24c8e71b5fc1be28655dbc0b3ef
psrok1 <pawel.srokosz@cert.pl>
2025-12-01 13:39:34 +0100

    Version bump: v2.16.0

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, CERT Polska'
 author = 'CERT Polska'
 
 # The full version, including alpha/beta/rc tags
-release = '2.15.1'
+release = '2.16.0'
 
 
 # -- General configuration ---------------------------------------------------

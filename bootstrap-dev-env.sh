b84abae7fb08300b57bd0b6eaa8325a426a3ee37
Sharon Yankovich <49753115+yankovs@users.noreply.github.com>
2026-01-12 12:42:22 +0200

    Use UV for package management (#1096)

---
+++ bootstrap-dev-env.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
+
+# Install pre-commit
+uv pip install pre-commit
+
+# Install pre-commit hooks
+pre-commit install --install-hooks
+
+# Install all python packages
+echo "Installing packages..."
+export UV_VENV_CLEAR=1
+uv venv
+uv sync --frozen
+uv sync --group dev
+uv sync --group test
+source .venv/bin/activate
